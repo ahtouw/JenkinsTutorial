662a8a5e83b1972193a993dab02fb35472cec5ff
William Ah Tou <wahtou.job@gmail.com>
2020-02-07 11:07:50 -0500

    Add files via upload

---
+++ project3-master/helloworld.py
@@ -0,0 +1,11 @@
+def func(a:int) -> int:
+    """Returns an integer a + 2
+    
+    Arguments:
+        a {int} -- any integer
+    
+    Returns:
+        int -- a + 2
+    """ 
+        
+    return a+2
